b22f168ccb95d0b817d658849f24be60d5e1c681
Hiroaki Nakamura <hnakamur@gmail.com>
2023-12-17 02:36:35 +0900

    Use glob in clang-format targets to format header files too

--- cmake/clang_format.cmake
+++ cmake/clang_format.cmake
@@ -2,9 +2,19 @@ set(CLANG_FORMAT
     clang-format
     CACHE STRING "Path to clang-format command"
 )
+set(CLANG_FORMAT_TARGET_EXTENTIONS
+    h;hpp;hh;c;cc;cxx;cpp
+    CACHE STRING "clang-format target filename extensions"
+)
 
-function(add_clang_format_target target files)
+function(add_clang_format_target target directory)
   find_program(CLANG_FORMAT_PATH ${CLANG_FORMAT} REQUIRED)
+  list(TRANSFORM CLANG_FORMAT_TARGET_EXTENTIONS PREPEND ${directory}/*. OUTPUT_VARIABLE patterns)
+  file(
+    GLOB_RECURSE files FOLLOW_SYMLINKS
+    LIST_DIRECTORIES false
+    ${patterns}
+  )
   add_custom_target(
     ${target}
     COMMAND ${CLANG_FORMAT_PATH} -i ${files}
--- cmake/cmake_format.cmake
+++ cmake/cmake_format.cmake
@@ -3,16 +3,16 @@ set(CMAKE_FORMAT
     CACHE STRING "Path to cmake-format command"
 )
 
-function(add_cmake_format_target target)
+function(add_cmake_format_target_toplevel target)
   find_program(CMAKE_FORMAT_PATH ${CMAKE_FORMAT} REQUIRED)
   add_custom_target(
     ${target}
     COMMAND ${CMAKE_FORMAT_PATH} -i ${CMAKE_CURRENT_SOURCE_DIR}/CMakeLists.txt
     VERBATIM
   )
-endfunction(add_cmake_format_target)
+endfunction(add_cmake_format_target_toplevel)
 
-function(add_cmake_format_target_recurse target directory)
+function(add_cmake_format_target target directory)
   find_program(CMAKE_FORMAT_PATH ${CMAKE_FORMAT} REQUIRED)
   set(patterns CMakeLists.txt;*.cmake)
   list(TRANSFORM patterns PREPEND ${directory}/)
@@ -26,4 +26,4 @@ function(add_cmake_format_target_recurse target directory)
     COMMAND ${CMAKE_FORMAT_PATH} -i ${files}
     VERBATIM
   )
-endfunction(add_cmake_format_target_recurse)
+endfunction(add_cmake_format_target)
